062944e48b9de166887dc3fa147a5c43fa1826e6
Mykola Humanov <morgenpurple@gmail.com>
2024-10-06 22:48:44 +0300

    examples/manipulate_html.rs: added

--- examples/manipulate_html.rs
+++ examples/manipulate_html.rs
@@ -10,10 +10,14 @@ fn main() {
                     <li>1</li>
                     <li>2</li>
                     <li>3</li>
-                    <li>4</li>
                 </ul>
-                <div class="other"></div>
-            </div
+            </div>
+            <div class="replace-it">
+                Replace me
+            </div>
+            <div class="remove-it">
+                Remove me
+            </div>
         </body>
     </html>"#;
 
@@ -23,16 +27,28 @@ fn main() {
     println!("{}", doc.html());
     println!("{:-<50}", "");
 
-    let mut content_selection = doc.select(".content");
+    let mut content_selection = doc.select("body .content");
     content_selection.append_html(r#"<div class="inner">inner block</div>"#);
 
     // After adding a new block to the content block
     println!("{}", doc.html());
     println!("{:-<50}", "");
 
+
+    content_selection.set_html(r#"<p>1,2,3</p>"#);
+    // After setting the content the new content
+    println!("{}", doc.html());
+    println!("{:-<50}", "");
+
+    doc.select(".remove-it").remove();
+
+    // After removing the remove-it div
+    println!("{}", doc.html());
+    println!("{:-<50}", "");
+
     // Replacing inner block content with new content, current selection remains the same
-    let mut inner_selection = doc.select(".other");
-    inner_selection.replace_with_html(r#"<div class="inner">INNER BLOCK</div>"#);
+    let mut replace_selection = doc.select(".replace-it");
+    replace_selection.replace_with_html(r#"<div class="replaced">Replaced</div>"#);
 
    // assert_eq!(inner_selection.text(), "inner block".into());
 
