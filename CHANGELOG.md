7e104b67351d12f737f0c64b26bbbe19768a9580
Mykola Humanov <morgenpurple@gmail.com>
2024-10-27 11:54:12 +0200

    switch from rustc-hash to foldhash

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,7 @@ All notable changes to the `dom_query` crate will be documented in this file.
 - Internal changes due to switch to `selectors` v0.26.0 switch.
 - `Selection` methods that required `&mut` now doesn't require `&mut`, finally.
 - Improve performance for `Document::from`, `Selection::select` and others.
+- Switched from using `rustc-hash` to `foldhash`.
 
 ### Added
 - Added `Node::ancestors` method, that allows to get all or limited number of ancestors of a node.
--- src/entities.rs
+++ src/entities.rs
@@ -3,17 +3,15 @@
 #[cfg(feature = "hashbrown")]
 mod inline {
     use hashbrown::HashSet;
-    use rustc_hash::FxHasher;
-    use std::hash::BuildHasherDefault;
-    pub type NodeIdSet = HashSet<crate::NodeId, BuildHasherDefault<FxHasher>>;
-    pub type HashSetFx<K> = HashSet<K, BuildHasherDefault<FxHasher>>;
+    pub type NodeIdSet = HashSet<crate::NodeId>;
+    pub type HashSetFx<K> = HashSet<K>;
 }
 
 #[cfg(not(feature = "hashbrown"))]
 mod inline {
-    use rustc_hash::FxHashSet;
-    pub type NodeIdSet = FxHashSet<crate::NodeId>;
-    pub type HashSetFx<K> = FxHashSet<K>;
+    use foldhash::HashSet;
+    pub type NodeIdSet = HashSet<crate::NodeId>;
+    pub type HashSetFx<K> = HashSet<K>;
 }
 
 pub(crate) use inline::{HashSetFx, NodeIdSet};
