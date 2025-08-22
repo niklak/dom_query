8cad8d3798b1697bf483f454d14502c3805fa544
Mykola Humanov <morgenpurple@gmail.com>
2025-08-22 11:13:55 +0300

    cargo fmt

--- tests/utils.rs
+++ tests/utils.rs
@@ -1,7 +1,5 @@
 #![allow(dead_code)]
 
-pub (crate) fn squash_whitespace(src: &str) -> String {
-    src
-        .split_whitespace()
-        .collect::<String>()
+pub(crate) fn squash_whitespace(src: &str) -> String {
+    src.split_whitespace().collect::<String>()
 }
