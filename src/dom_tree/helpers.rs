127cdb27ce94faf0521ef1660397903c4166f6bb
Mykola Humanov <morgenpurple@gmail.com>
2025-08-25 15:57:21 +0300

    src/dom_tree/helpers.rs: revise `normalized_char_count`

--- src/dom_tree/helpers.rs
+++ src/dom_tree/helpers.rs
@@ -3,10 +3,11 @@ pub(crate) fn normalized_char_count(text: &str, start_whitespace: bool) -> usize
     let mut prev_was_whitespace = start_whitespace;
 
     for c in text.chars() {
-        if !(prev_was_whitespace && c.is_whitespace()) {
+        let is_ws = c.is_whitespace();
+        if !(prev_was_whitespace && is_ws) {
             char_count += 1;
         }
-        prev_was_whitespace = c.is_whitespace();
+        prev_was_whitespace = is_ws;
     }
     char_count
 }
