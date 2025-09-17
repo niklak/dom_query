2bf4b70d4f1d21ff0cb8e6e84c57bbff2d74aff7
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2025-09-17 11:28:45 +0200

    Fix code lint

--- src/lib.rs
+++ src/lib.rs
@@ -39,4 +39,4 @@ pub use node::{Element, Node, NodeData, NodeId, NodeIdProver, NodeRef};
 pub use selection::Selection;
 
 // re-export
-pub use html5ever::{LocalName, local_name};
+pub use html5ever::{local_name, LocalName};
