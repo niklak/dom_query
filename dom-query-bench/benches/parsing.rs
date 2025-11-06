03ecc2d13f8986688e090295db1b46281b754cac
Mykola Humanov <morgenpurple@gmail.com>
2025-11-06 13:35:33 +0200

    dom-query-bench/benches/parsing.rs: add bench for parsing

---
+++ dom-query-bench/benches/parsing.rs
@@ -0,0 +1,28 @@
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use std::{hint::black_box, time::Duration};
+
+use dom_query::Document;
+
+fn bench_parsing(c: &mut Criterion) {
+    let contents = include_str!("../test-pages/rustwiki_2024.html");
+
+    let mut group = c.benchmark_group("dom_query");
+
+    group.bench_with_input(
+        BenchmarkId::new("parsing", "simple"),
+        contents,
+        |b, contents| {
+            b.iter(|| {
+                let document = Document::from(contents);
+                black_box(document)
+            })
+        },
+    );
+
+    group.warm_up_time(Duration::from_secs(5));
+    group.measurement_time(Duration::from_secs(15));
+
+    group.finish();
+}
+criterion_group!(benches, bench_parsing);
+criterion_main!(benches);
